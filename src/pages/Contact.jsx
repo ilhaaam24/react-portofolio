6799c523a82256bbc8fb8f61b4caf3736c59c54a
Ilham <mi1797128@gmail.com>
2024-09-05 09:10:25 +0700

    feat:email and sweetalert

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import { MdOutlineEmail } from "react-icons/md";
 import { BsFillTelephoneFill } from "react-icons/bs";
+import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 const Contact = () => {
+
+const form = useRef();
+  
+const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_3nynqjh", "template_ilt2syc", form.current, {
+        publicKey: "iZBcvU_iu6YuXcPqd",
+      })
+      .then(
+        () => {
+          Swal.fire({
+            position: "center",
+            icon: "success",
+            title: "Your message has been sent!",
+            showConfirmButton: false,
+            timer: 1500,
+          });
+          console.log("Pesan berhasil dikirim!");
+          console.log(e.target[0].value);
+          console.log(e.target[1].value);
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+
   return (
-    <div id="contact" className="flex flex-col items-center justify-center min-h-screen gap-16">
+    <div id="contact" className="flex flex-col items-center justify-center min-h-screen gap-8">
       <h1 className="text-4xl font-bold">Get in touch </h1>
       <div className="flex flex-col md:flex-row justify-between gap-8 md:gap-16 p-4">
         <div className="flex flex-col gap-4 md:gap-8">
@@ -18,10 +49,11 @@ const Contact = () => {
             <p>+6281615697675</p>
           </div>
         </div>
-        <form action="" className="flex flex-col gap-4 md:gap-6 items-center w-full md:w-auto">
-          <input type="text" placeholder="Full Name" className="p-3 rounded-md w-full md:w-[400px] outline-none text-[#697565]" />
-          <textarea name="message" id="message" cols="40" rows="5" placeholder="Your Message" className="p-3 rounded-md w-full md:w-[400px] outline-none text-[#697565]"></textarea>
-          <button className="bg-[#697565] text-white px-6 py-2 rounded-lg w-full md:w-28">Send</button>
+        <form onSubmit={sendEmail} ref={form} className="flex flex-col gap-4 md:gap-6 items-center w-full md:w-auto">
+          <input type="text" name="user_name" id="user_name" placeholder="Full Name" className="p-3 rounded-md w-full md:w-[400px] outline-none text-[#697565]"  required/>
+          <input type="email" name="user_email" id="user_email" placeholder="Email" className="p-3 rounded-md w-full md:w-[400px] outline-none text-[#697565]"  required/>
+          <textarea name="message" id="message" cols="40" rows="5" placeholder="Your Message" className="p-3 rounded-md w-full md:w-[400px] outline-none text-[#697565]" required></textarea>
+          <button type="submit" className="bg-[#697565] text-white px-6 py-2 rounded-lg w-full md:w-28">Send</button>
         </form>
       </div>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="flex  flex-col-reverse  py-20 px-6 gap-8 h-auto   lg:flex-row md:px-32 lg:justify-evenly lg:items-center  ">
+    <div className="flex  flex-col-reverse  py-20 px-6 gap-8 h-auto min-h-screen  lg:flex-row md:px-32 lg:justify-evenly lg:items-center  ">
       <div className="flex-1 flex flex-col justify-center items-start px-4">
         <p className="text-4xl font-bold md:text-5xl">Hey, I'am a</p>
         <h1 className="text-4xl mt-3 font-bold md:text-5xl ">Fullstack Developer</h1>
-        <button className="bg-[#697565] text-white px-4 py-2 rounded-md mt-4 hover:bg-[#8f8f8f]">Hire Me</button>
+        <a href="#contact">
+          <button className="bg-[#697565] text-white px-4 py-2 rounded-md mt-4 hover:bg-[#8f8f8f]">Hire Me</button>
+        </a>
       </div>
       <div className="flex-1 flex justify-center items-center">
         <img src="https://cdn.pixabay.com/photo/2020/05/17/20/21/cat-5183427_1280.jpg" alt="Profile" className="rounded-full h-[300px] w-[300px] md:h-[350px] md:w-[350px] lg:h-[450px] lg:w-[450px] object-cover" />
